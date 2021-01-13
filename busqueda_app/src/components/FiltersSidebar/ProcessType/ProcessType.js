4ac8a96902dadee44cad1511a28021f30fa2e7a9
jarogu90 <jaime.romo@apptivatech.com>
2021-01-13 14:26:01 +0100

    checkbox filtros sidebar

--- busqueda_app/src/components/FiltersSidebar/ProcessType/ProcessType.js
+++ busqueda_app/src/components/FiltersSidebar/ProcessType/ProcessType.js
@@ -19,6 +19,25 @@ import ProcessTypeIcon from "../../../img/processTypeIcon";
 const ProcessType = ({ showing }) => {
   const state = useContext(GlobalStateContext);
   const dispatch = useContext(GlobalDispatchContext);
+
+  const RefinementOption = (props) => {
+    console.log(props);
+    return (
+      <div
+        className={props.bemBlocks
+          .option()
+          .state({ selected: props.selected })
+          .mix(props.bemBlocks.container("item"))}
+        onClick={props.onClick}
+      >
+        <div className={props.bemBlocks.option("text")}>{props.label}</div>
+
+        <div className={props.bemBlocks.option("count")}>
+          <input type="checkbox" />
+        </div>
+      </div>
+    );
+  };
   return (
     <div className="dropdown-container">
       <input type="checkbox" id="drop_pt" />
@@ -40,6 +59,7 @@ const ProcessType = ({ showing }) => {
           showCount={false}
           orderKey="_term"
           orderDirection="asc"
+          itemComponent={RefinementOption}
         />
       </div>
       {state.show ? <div className="line"></div> : null}
